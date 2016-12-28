2236cdadfefc973ef0f64025cb40fb47574a66d1
dudin <sihabudinnaufal@gmail.com>
2016-12-28 10:36:51 -0500

    dudin: projeck

---
+++ common.mk
@@ -0,0 +1,231 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := device/zte/common
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libalsa-intf \
+    libaudio-resampler \
+    libaudioutils \
+    aplay \
+    amix \
+    arec \
+    alsaucm_test \
+    tinymix
+
+# Misc
+PRODUCT_PACKAGES += \
+    curl \
+    libbson \
+    libcurl \
+    tcpdump
+
+# Charger
+PRODUCT_PACKAGES += charger_res_images
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Crda
+PRODUCT_PACKAGES += \
+    crda \
+    linville.key.pub.pem \
+    regdbdump \
+    regulatory.bin
+
+# Stlport
+PRODUCT_PACKAGES += \
+    libstlport
+
+# Compatibility symbols wrappers
+PRODUCT_PACKAGES += \
+    libboringssl-compat \
+    libmoto_log
+
+# Qcom SoftAP
+PRODUCT_PACKAGES += \
+    libQWiFiSoftApCfg
+
+# Wifi
+PRODUCT_PACKAGES += \
+    p2p_supplicant_overlay.conf \
+    wpa_supplicant_overlay.conf
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.crda.sh \
+    init.qcom.bt.sh \
+    init.qcom.coex.sh \
+    init.qcom.usb.sh
+
+# Thermal profiles
+PRODUCT_PACKAGES += \
+    thermald-8960.conf
+
+# Scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/scripts/mount_pds.sh:system/bin/mount_pds.sh
+
+# We have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# QCOM Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=1 \
+    debug.sf.hw=1 \
+    debug.composition.type=dyn \
+    persist.hwc.mdpcomp.enable=true \
+    debug.mdpcomp.logs=0 \
+    debug.enabletr=0
+
+# QCOM Display
+PRODUCT_PACKAGES += \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer
+
+# Omx
+PRODUCT_PACKAGES += \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libOmxCore \
+    libstagefrighthw \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    e2fsck \
+    resize2fs \
+    setup_fs
+
+# WiFi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd.accept \
+    hostapd.deny \
+    hostapd_default.conf \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Symlinks
+PRODUCT_PACKAGES += \
+    libxml2 \
+    WCNSS_qcom_wlan_nv.bin
+
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+# QCOM
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    ro.qc.sdk.audio.ssr=false \
+    ro.audio.flinger_standbytime_ms=300
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.hfp.ver=1.6 \
+    ro.qualcomm.bluetooth.sap=true \
+    ro.qualcomm.bt.hci_transport=smd \
+    ro.bluetooth.request.master=true \
+    ro.bluetooth.remote.autoconnect=true
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=true \
+    qcom.hw.aac.encoder=true \
+    persist.audio.lowlatency.rec=false
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.qc.sub.rstrtlvl=3 \
+    persist.sys.qc.sub.rdump.on=1 \
+    persist.sys.qc.sub.rdump.max=20
+
+# Radio and Telephony
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    keyguard.no_require_sim=true \
+    ro.use_data_netmgrd=true \
+    ro.ril.transmitpower=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.call_type=1 \
+    ro.config.vc_call_vol_steps=7 \
+    ro.modem.no_wdog_chk=1
+
+PRODUCT_GMS_CLIENTID_BASE ?= android-zte
+
+# QC time services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
